9567caddaa83c6fa6c637befe8fbd41bcf1503d9
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2023-02-13 22:21:33 -0300

    Update patch.js

--- patch.js
+++ patch.js
@@ -2,10 +2,10 @@
             const __define = define;
             globalThis.define = function(params, callback) {
                 return __define(params, function(...defargs) {
-                    const proto801 = defargs[params.indexOf("./reseau/ProtoM801")]?.default;
-                    const module801 = defargs[params.indexOf("./Module801")]?.default;
-                    const interfaceTalents = defargs[params.indexOf("../interfaces/InterfaceTalents")]?.default;
-                    const boutonEtat = defargs[params.indexOf(params.find(p => p.endsWith("I_BoutonEtat")))]?.I_BoutonEtat;
+                    const proto801 = (defargs[params.indexOf("./reseau/ProtoM801")] || {}).default;
+                    const module801 = (defargs[params.indexOf("./Module801")] || {}).default;
+                    const interfaceTalents = (defargs[params.indexOf("../interfaces/InterfaceTalents")] || {}).default;
+                    const boutonEtat = (defargs[params.indexOf(params.find(p => p.endsWith("I_BoutonEtat")))] || {}).I_BoutonEtat;
                     
                     if (boutonEtat) {
                         boutonEtat.prototype.cocher = function (OUI, DECLENCHER) {
